69fae054cffd5757037ef74749f4f35bb38af1b1
Michelle Poterek <poterekm@gmail.com>
2017-08-11 10:21:02 -0700

    appropriately display questions from pseudo model on index template

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
   var questions = [{
     id : 1,
     username: "codecutie101",
     category: "basic question",
-    title: "How do I type?"
-    question: "When my fingers move nothing happens"
+    title: "How do I type?",
+    body: "When my fingers move nothing happens"
   }, {
     id : 2,
     username: "notanumber_downWithCapitalizzzm",
     category: "basic question",
-    title: "How do I turn my computer on?"
-    question: "IDk, i'm like using my phone right now. Can I code from there?"
+    title: "How do I turn my computer on?",
+    body: "IDk, i'm like using my phone right now. Can I code from there?"
   }];
+
   export default Ember.Route.extend({
   model() {
     return questions;
