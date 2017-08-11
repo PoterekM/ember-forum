1a1cf2d9928beb0e53697200661fd989a4ef3e69
Michelle Poterek <poterekm@gmail.com>
2017-08-11 10:44:02 -0700

    update model hook to be linked to firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,7 @@
 import Ember from 'ember';
 
-  var questions = [{
-    id : 1,
-    username: "codecutie101",
-    category: "basic question",
-    title: "How do I type?",
-    body: "When my fingers move nothing happens"
-  }, {
-    id : 2,
-    username: "notanumber_downWithCapitalizzzm",
-    category: "basic question",
-    title: "How do I turn my computer on?",
-    body: "IDk, i'm like using my phone right now. Can I code from there?"
-  }];
-
-  export default Ember.Route.extend({
+export default Ember.Route.extend({
   model() {
-    return questions;
+    return this.store.findAll('question');
   },
 });
