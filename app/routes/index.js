8f31bcb40b7dd8b6336af086fc2d75bb0bc54819
Michelle Poterek <poterekm@gmail.com>
2017-08-11 10:10:10 -0700

    add seeded data to model for questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  var questions = [{
+    id : 1,
+    username: "codecutie101",
+    category: "basic question",
+    title: "How do I type?"
+    question: "When my fingers move nothing happens"
+  }, {
+    id : 2,
+    username: "notanumber_downWithCapitalizzzm",
+    category: "basic question",
+    title: "How do I turn my computer on?"
+    question: "IDk, i'm like using my phone right now. Can I code from there?"
+  }];
+  export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
