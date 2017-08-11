ac05daafee982d6b2d81dd6093aa6cfca6d7652e
Michelle Poterek <poterekm@gmail.com>
2017-08-11 11:24:52 -0700

    add component behavior of save button for new question form

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        username: this.get('username'),
+        category: this.get('category'),
+        title: this.get('title'),
+        body: this.get('body'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
